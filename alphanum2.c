31e870787d47c2193943a967de16fd6d3a6b9c1c
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2018-03-13 15:56:25 +0530

    Create alphanum2.c

---
+++ alphanum2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include<string.h>
+int main(void)
+{
+	char a[20];
+	int b,x=0,y=0;
+	scanf("%s",a);
+	for(b=0;a[b]!='\0';b++)
+	{
+		if((a[b]>='0')&&(a[b]<='9'))
+		{
+			x++;
+		}
+		if((a[b]>='a')&&(a[b]<='z'))
+		{
+			y++;
+		}
+	}
+	if((x>=1)&&(y>=0))
+	{
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+
+	return 0;
+}
