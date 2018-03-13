6023112ad5e582b31b279868e168ace3e5d04708
bhoomavsb <31505040+bhoomavsb@users.noreply.github.com>
2018-03-13 16:42:55 +0530

    Create greatest.c

---
+++ greatest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i,a,b,d;
+	scanf("%d %d",&a,&b);
+	for(i=1;(i<=a&&i<=b);i++)
+	{
+		if((a%i==0)&&(b%i==0))
+		d=i;
+	}
+	printf("%d",d);
+	return 0;
+}
